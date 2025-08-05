a5f497260f570d7d8c44e619189bb7ca9dd487b9
Anagha-07 <anaghapajit@gmail.com>
2025-08-05 20:44:39 +0000

    Update greeting message for Jenkins test

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello from Jenkins Pipeline!"
+def index():
+    return """
+    <h2 style="color:green;">✅ SUCCESS</h2>
+    <p>🚀 Hello from <strong>Jenkins Pipeline!</strong></p>
+    <p>📦 Build & Deployment Completed</p>
+    """
+
+@app.route('/health')
+def health():
+    return """
+    <h3>Health Check</h3>
+    <ul>
+        <li>Status: <strong>UP</strong></li>
+        <li>Service: Flask Jenkins Demo</li>
+    </ul>
+    """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
